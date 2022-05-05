05d1e5f824bf75ed74e0966bd5de6715771764a7
AhmadEleiwa <ahmadilawa@gmail.com>
2022-05-05 10:31:04 +0300

    next step

--- src/chat/components/chat-view/MessageCard.js
+++ src/chat/components/chat-view/MessageCard.js
@@ -8,7 +8,8 @@ const MessageCard = props =>{
     let content = '' 
     let element  = [];
     content = props.children
-    if(content.search('www.youtube.com/watch')>0){
+
+    if(content.search('www.youtube.com/watch')>=0){
     //    content= content.replace('watch?v=', 'embed/')
        let link = content.substring(content.search('https://www.youtube.com/watch') , content.length)
        let frame = link.replace('watch?v=', 'embed/')
@@ -16,6 +17,20 @@ const MessageCard = props =>{
        element.push(<a  href={link} target='_blank'>{link}</a>)
        element.push(<p>{content.replace(link, '')}</p>) 
        
+    }
+    else if(content.includes('https') && content.includes('com')){
+        let link = content.substring(content.search('https'),content.search('com')+3)
+        let c = content.split(link)
+        console.log(c)      
+        element.push(<p>{c[0]}<a  href={link} target='_blank'>{link}</a>{c[1]}</p>)
+
+    }
+    else if(content.includes('http') && content.includes('com')){
+        let link = content.substring(content.search('http'),content.search('com')+3)
+        let c = content.split(link)
+        console.log(c)      
+        element.push(<p>{c[0]}<a  href={link} target='_blank'>{link}</a>{c[1]}</p>)
+
     }
     else{
         element.push(<p>{props.children}</p>)
